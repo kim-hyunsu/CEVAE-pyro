09c5f2fc57b3ece99175153055fba91ed5c2f0b5
kim-hyunsu <fordirac@naver.com>
2019-06-07 21:28:37 +0900

    IHDPLoader

--- data_loader.py
+++ data_loader.py
@@ -1,20 +1,54 @@
 import numpy as np
-import torch
+import torch.utils.data
+
+
+class IHDPDataset(torch.utils.data.Dataset):
+    def __init__(self, np_data):
+        self.mu1 = np_data['mu1']
+        self.mu0 = np_data['mu0']
+        self.t = np_data['t']
+        self.x = np_data['x']
+        self.yf = np_data['yf']
+        self.ycf = np_data['ycf']
+        self.len = self.x.shape[0]
+
+    def __getitem__(self, idx):
+        return self.mu1[idx], self.mu0[idx], self.t[idx], self.x[idx], self.yf[idx], self.ycf[idx]
+
+    def __len__(self):
+        return self.len
 
 
 class IHDPLoader(object):
-    def __init__(self, path="data/IHDP", cuda=False):
+    def __init__(self, path="data/IHDP-np", cuda=False):
         self.path = path
-        self.binary_features = list(range(6, 25))
-        self.continuous_features = list(range(7))
+        self.cuda = cuda
 
-        for i in range(10):
-            data = np.loadtxt(
-                f'{self.path}/ihdp_npci_{i % 10 + 1}.csv', delimiter=',')
-            # TODO
+        test_data = np.load(f'{path}/ihdp_npci_1-100.test.npz')
+        train_data = np.load(f'{path}/ihdp_npci_1-100.train.npz')
 
-    def num_each_features(self):
-        return self.binary_features, self.continuous_features
+        self._indices_binary_continuous(train_data['x'][1, :])
+        self.test_set = IHDPDataset(test_data)
+        self.train_set = IHDPDataset(train_data)
+
+    def _indices_binary_continuous(self, features):
+        binary_indices = []
+        continuous_indices = []
+        for i, x in enumerate(features):
+            binary = True
+            for num in x:
+                if not num in (0, 1, 2):
+                    binary = False
+            if binary:
+                binary_indices.append(i)
+            else:
+                continuous_indices.append(i)
+
+        self.binary_indices = binary_indices
+        self.continuous_indices = continuous_indices
+
+    def indices_each_features(self):
+        return self.binary_indices, self.continuous_indices
 
     def train_loader(self, batch_size):
         train_loader = torch.utils.data.DataLoader(
@@ -24,9 +58,7 @@ class IHDPLoader(object):
 
     def test_loader(self, batch_size):
         test_loader = torch.utils.data.DataLoader(
-            dataset=self.test_set, batch_size=batch_size, shuffle=False, {
-                'num_workers': 1, 'pin_memory': self.cuda
-            })
+            dataset=self.test_set, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=self.cuda)
 
         return test_loader
 
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def main(*args, **kwargs):
-    pass
-
-
 if __name__ == "__main__":
     # command arguments
-    main()
+
+    data = IHDPLoader()
